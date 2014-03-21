34c60332efe0224532778dc105cb3ff440722892
jessenpan <jessenpan@gmail.com>
2014-03-21 13:13:46 +0800

    add utility class

---
+++ DAL/BJ_DAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ZYNLPJXT.Entity;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using ZYNLPJXT.Utility;
+
+namespace ZYNLPJXT.DAL
+{
+    //班级 数据访问层
+    public class BJ_DAL
+    {
+        public BJ getBJbyBH(int id) { 
+            
+            //string sql = "select * from BJ where BJBH='" + id + "'";
+            string sql = "select * from tabone";
+            BJ bj=new BJ();
+            string col1 = null, col2 = null;
+            DbConnection dbCon = new DbConnection();
+            SqlDataReader reader = dbCon.executeQuery(sql);
+            if (reader.Read())
+            {
+                col1 = reader.GetString(0);
+                col2 = reader.GetString(1);
+            }
+            bj.Bjmc = col1;
+            reader.Close();
+            return bj;
+        }
+    }
+}
---
+++ Utility/DbConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace ZYNLPJXT.Utility
+{
+    public class DbConnection
+    {
+         private static string SqlConUrl = ConfigurationManager.AppSettings["connectionStr"];
+
+
+        //使用datareader关闭连接
+        public SqlDataReader executeQuery(string sql) {
+
+            SqlConnection con = new SqlConnection(SqlConUrl);
+            con.Open();
+            SqlCommand sqlCom = new SqlCommand(sql,con);
+            SqlDataReader reader = sqlCom.ExecuteReader(CommandBehavior.CloseConnection) ;                
+            return reader;
+        }
+       
+              
+    }
+}
