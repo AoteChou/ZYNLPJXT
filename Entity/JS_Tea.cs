6a5ee64710f36e72fb537ec383a5d17d94a613b5
jessenpan <jessenpan@gmail.com>
2014-03-24 20:15:17 +0800

    add  是否是学科负责人和是否是课程负责人到类JS_Tea

--- Entity/JS_Tea.cs
+++ Entity/JS_Tea.cs
@@ -11,6 +11,26 @@ namespace ZYNLPJXT.Entity
         //教师编号
         private string jsbh;
 
+        //是否是学科负责人
+        private bool sfsxkfzr;
+
+        public bool Sfsxkfzr
+        {
+            get { return sfsxkfzr; }
+            set { sfsxkfzr = value; }
+        }
+
+
+        //是否是课程负责人
+        private bool sfskcfzr;
+
+        public bool Sfskcfzr
+        {
+            get { return sfskcfzr; }
+            set { sfskcfzr = value; }
+        }
+
+
         public string Jsbh
         {
             get { return jsbh; }
